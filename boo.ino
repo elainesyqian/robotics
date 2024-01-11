3d8f849bad55a5ae5471c42ec415f8486d51f2ca
elaine! <110132709+elainesyqian@users.noreply.github.com>
2024-01-10 22:03:52 -0500

    updated lcd pins

--- boo.ino
+++ boo.ino
@@ -1,14 +1,16 @@
-//Food for Thought
-//Eileen Kuang and Elaine Qian
+// Food for Thought
+// Eileen Kuang and Elaine Qian
 
 // Include libraries for servo, lcd, and keypad
 #include "Servo.h"
 #include <LiquidCrystal.h>
 #include <Keypad.h>
 
-//need to make like 4 of these lolol
-// Create a new servo object:
-Servo myservo;
+// Create servo objects:
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
 
 // keypad
 const int ROW_NUM = 4; // four rows
@@ -21,20 +23,22 @@ char keys[ROW_NUM][COLUMN_NUM] = {
   {'*','0','#', 'D'}
 };
 
-byte pin_rows[ROW_NUM] = {A7, A6, A5, A4}; //connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {A3, A2, A1, A0}; //connect to the column pinouts of the keypad
+byte pin_rows[ROW_NUM] = {14, 15, 16, 17}; //connect to the row pinouts of the keypad
+byte pin_column[COLUMN_NUM] = {18, 19, 20, 21}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM ); // initializing keypad object
 
 // lcd display
-const int RS = 12, EN = 11, D4 = 5, D5 = 4, D6 = 3, D7 = 2;
+const int RS = A0, EN = A1, D4 = A2, D5 = A3, D6 = A4, D7 = A5;
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 // piezo!
-const int PIE = 10;
+const int PIE = A15;
 
 int num1, num2, op, ans, input;
-char key;
+int wrong=0; // counts consecutive wrong answers
+char key, choice;
+long time = 300000; // five minutes in milliseconds
 
 void setup() {
   Serial.begin(9600);
@@ -43,12 +47,17 @@ void setup() {
   lcd.begin(16, 2); //sets up rows and columns on the screen
   lcd.clear();
 
-  myservo.attach(12);
+  myservo1.attach(22);
 
-  myservo.write(0);
-  delay(1000);
-  myservo.write(180);
-  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("   Welcome to");
+  lcd.setCursor(0,1);
+  lcd.print(" Food 4 Thought!");
+
+  myservo1.write(100);
+  delay(3000);
+  myservo1.write(90);
 
 }
 
@@ -65,13 +74,13 @@ void generateProblem(){
     num1 = random(10, 100);
     num2 = random(10, 100);
 
-    if (op == 1){
+    if (op == 1)  {
       ans = num1 + num2;
-    } else if (op == 2){
+    } else if (op == 2) {
       ans = num1 - num2;
-    } else if (op == 3){
+    } else if (op == 3) {
       ans = num1 * num2;
-    } else if (op == 4){
+    } else if (op == 4) {
       ans = (int) num1 / num2;
     }
 
@@ -80,9 +89,8 @@ void generateProblem(){
         if(ans * num2 == num1){
           break;
         }
-      } else {
-        break;
-      }
+      } else
+          break;
     }
 
   }
@@ -107,7 +115,8 @@ void printProblem(){
   lcd.print("?");
 }
 
-bool getAns(){
+bool getAns() {
+
   lcd.setCursor(0,1);
 
   key = keypad.getKey();
@@ -129,4 +138,85 @@ bool getAns(){
   lcd.print(key);
 
   return(input == ans);
+
+  dispenseOrNot();
+
+}
+
+void dispenseOrNot() {
+
+  // if user answers correctly
+  if(getAns()){
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+
+    lcd.print("Enter your choice: ");
+
+    // receive user input
+    key = keypad.getKey();
+
+    while(key == NO_KEY) {
+      key = keypad.getKey();
+    }
+
+    choice = key;
+    lcd.print(key);
+
+    key = keypad.getKey();
+
+    while(key == NO_KEY) {
+      key = keypad.getKey();
+    }
+
+    input = input + (key-'0');
+
+    lcd.setCursor(0,1);
+    lcd.print(key);
+
+    // dispense snack based on user choice
+    switch(key){
+
+      case 'A':
+        myservo1.write(90);
+        break;
+
+      case 'B':
+        myservo2.write(90);
+        break;
+
+      case 'C':
+        myservo3.write(90);
+        break;
+
+      case 'D':
+        myservo4.write(90);
+        break;
+
+    }
+
+    wrong=0;
+
+  }
+  // if user's answer is incorrect
+  else{
+
+    wrong++;
+    timeOut(wrong);
+
+  }
+
+}
+
+void timeOut(int wrong){
+
+  if(wrong > 3){
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("You're timed out!");
+    delay(time);
+
+  }
+
 }
