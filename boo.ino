fdf390cab0215a71b0c2374b8b6e8568011f61d4
elaine! <110132709+elainesyqian@users.noreply.github.com>
2024-01-15 11:17:09 -0500

    basically done (except for testing servos)

--- boo.ino
+++ boo.ino
@@ -5,6 +5,7 @@
 #include "Servo.h"
 #include <LiquidCrystal.h>
 #include <Keypad.h>
+#include <EEPROM.h>
 
 // Create servo objects:
 Servo myservo1;
@@ -42,55 +43,118 @@ long time = 300000; // five minutes in milliseconds
 
 void setup() {
   Serial.begin(9600);
-  delay(1000);
+  delay(2000);
 
   lcd.begin(16, 2); //sets up rows and columns on the screen
-  lcd.clear();
-
-  myservo1.attach(22);
-
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("   Welcome to");
   lcd.setCursor(0,1);
   lcd.print(" Food 4 Thought!");
 
-  myservo1.write(100);
-  delay(3000);
+  myservo1.attach(50);
+  // myservo1.write(100);
+  // delay(3000);
   myservo1.write(90);
+
+  myservo2.attach(51);
+  // myservo2.write(100);
+  // delay(3000);
+  myservo2.write(85);
+
+  myservo3.attach(52);
+  myservo3.write(90);
+
+  myservo4.attach(53);
+  myservo4.write(90);
+
+  delay(1500);
+  lcd.clear();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  startingPage();
+
+  while (wrong < 3){
+    generateProblem();
+    printProblem();
+    if (getAns()) {
+      dispense();
+      wrong = 0;
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Enjoy your snack!");
+      delay(5000);
+      startingPage();
+    } else {
+      wrong++;
+    }
+  }
+
+  timeOut();
+  wrong = 0;
+}
+
+void startingPage(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" Press any key");
+  lcd.setCursor(0,1);
+  lcd.print("  to start :)");
+
+  key = keypad.getKey();
+
+  while(key == NO_KEY) {
+    key = keypad.getKey();
+  }
+  delay(200);
+  lcd.clear();
+  delay(300);
 }
 
 void generateProblem(){
-  
-  op = random(1, 5);
 
-  while(true){
-    num1 = random(10, 100);
-    num2 = random(10, 100);
+  bool generate = true;
+
+  while(generate){
+    reseedRandom();
+
+    op = (int) random(1, 5);
 
     if (op == 1)  {
+      num1 = (int) random(10, 100);
+      num2 = (int) random(10, 100);
       ans = num1 + num2;
     } else if (op == 2) {
+      num1 = (int) random(10, 100);
+      num2 = (int) random(10, 100);
       ans = num1 - num2;
     } else if (op == 3) {
+      num1 = (int) random(2, 10);
+      num2 = (int) random(5, 10);
       ans = num1 * num2;
     } else if (op == 4) {
+      num1 = (int) random(10, 100);
+      num2 = (int) random(2, 9);
       ans = (int) num1 / num2;
     }
 
     if(ans < 100 && ans > 10){
       if(op == 4){
         if(ans * num2 == num1){
-          break;
+          generate = false;
+          lcd.print(num1);
+          lcd.print(num2);
+
         }
-      } else
-          break;
+      } else {
+        generate = false;
+        lcd.print(num1);
+        lcd.print(num2);
+
+      }
     }
 
   }
@@ -113,6 +177,7 @@ void printProblem(){
     }
   lcd.print(num2);
   lcd.print("?");
+  delay(10);
 }
 
 bool getAns() {
@@ -136,87 +201,105 @@ bool getAns() {
 
   input = input + (key-'0');
   lcd.print(key);
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+
+  if (input == ans){
+    lcd.print("Correct!");
+    delay(2000);
+  } else {
+    lcd.print("Incorrect >:((");
+    delay(2000);
+  }
 
   return(input == ans);
 
-  dispenseOrNot();
-
 }
 
-void dispenseOrNot() {
+void dispense() {
 
-  // if user answers correctly
-  if(getAns()){
+  lcd.clear();
+  lcd.setCursor(0,0);
 
-    lcd.clear();
-    lcd.setCursor(0,0);
+  lcd.print("Enter ur choice: ");
 
-    lcd.print("Enter your choice: ");
+  // receive user input
+  key = keypad.getKey();
 
-    // receive user input
+  while(key == NO_KEY) {
     key = keypad.getKey();
+  }
 
-    while(key == NO_KEY) {
-      key = keypad.getKey();
-    }
-
-    choice = key;
-    lcd.print(key);
-
-    key = keypad.getKey();
-
-    while(key == NO_KEY) {
-      key = keypad.getKey();
-    }
-
-    input = input + (key-'0');
-
-    lcd.setCursor(0,1);
-    lcd.print(key);
+  choice = key;
+  lcd.setCursor(0,1);
+  lcd.print(choice);
 
     // dispense snack based on user choice
-    switch(key){
+  switch(choice){
 
-      case 'A':
-        myservo1.write(90);
-        break;
+    case 'A':
+      myservo1.write(100);
+      delay(3000);
+      myservo1.write(90);
+      break;
 
-      case 'B':
-        myservo2.write(90);
-        break;
+    case 'B':
+      myservo2.write(95);
+      delay(3000);
+      myservo2.write(85);
+      break;
 
-      case 'C':
-        myservo3.write(90);
-        break;
+    case 'C':
+      myservo3.write(100);
+      delay(3000);
+      myservo3.write(90);
+      break;
 
-      case 'D':
-        myservo4.write(90);
-        break;
-
+    case 'D':
+      myservo4.write(100);
+      delay(3000);
+      myservo4.write(90);
+      break;
     }
-
-    wrong=0;
-
-  }
-  // if user's answer is incorrect
-  else{
-
-    wrong++;
-    timeOut(wrong);
-
-  }
 
 }
 
-void timeOut(int wrong){
+void timeOut(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("You're timed out!");
+  delay(time);
+}
 
-  if(wrong > 3){
-
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("You're timed out!");
-    delay(time);
-
+void reseedRandom( void )
+{
+  static const uint32_t HappyPrime = 937;
+  union
+  {
+    uint32_t i;
+    uint8_t b[4];
+  }
+  raw;
+  int8_t i;
+  unsigned int seed;
+  
+  for ( i=0; i < sizeof(raw.b); ++i )
+  {
+    raw.b[i] = EEPROM.read( i );
   }
 
+  do
+  {
+    raw.i += HappyPrime;
+    seed = raw.i & 0x7FFFFFFF;
+  }
+  while ( (seed < 1) || (seed > 2147483646) );
+
+  randomSeed( seed );  
+
+  for ( i=0; i < sizeof(raw.b); ++i )
+  {
+    EEPROM.write( i, raw.b[i] );
+  }
 }
